4651026c7dd2e0c1ebe4662820cc40be942ecf01
Jonnathan Ribeiro <jrib_st@outlook.com>
2024-07-24 21:23:17 -0300

    refactor: remove check for private repository
    This commit removes the check that throws an error if the
    repository is not found or is private. This change is intended to
    improve the system's handling of user repositories by no longer
    failing when encountering private repositories.

--- src/fetchers/repo-fetcher.js
+++ src/fetchers/repo-fetcher.js
@@ -92,7 +92,7 @@ const fetchRepo = async (username, reponame) => {
   const isOrg = data.user === null && data.organization;
 
   if (isUser) {
-    if (!data.user.repository || data.user.repository.isPrivate) {
+    if (!data.user.repository /* || data.user.repository.isPrivate */) {
       throw new Error("User Repository Not found");
     }
     return {
@@ -103,8 +103,8 @@ const fetchRepo = async (username, reponame) => {
 
   if (isOrg) {
     if (
-      !data.organization.repository ||
-      data.organization.repository.isPrivate
+      !data.organization.repository /* ||
+    data.organization.repository.isPrivate */
     ) {
       throw new Error("Organization Repository Not found");
     }
